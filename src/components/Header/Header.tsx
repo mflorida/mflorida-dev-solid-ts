3f570993ca14bd3482d74104c312b18ec9c27d4d
Mark M. Florida <4173034+mflorida@users.noreply.github.com>
2023-10-08 17:17:28 -0500

    Support CSS modules - with example implementation.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,9 @@
 import { content } from '../../content.tsx';
-import './style.scss';
+import style from './style.module.scss';
 
 export function Header() {
   return (
-    <h1 id={`header`}>
+    <h1 id={`header`} class={style.header}>
       <span>{content.header.name}</span>
       {' '}
       <span>{content.header.title}</span>
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,16 @@
-import { defineConfig } from 'vite'
-import solid from 'vite-plugin-solid'
+import { defineConfig } from 'vite';
+import solid from 'vite-plugin-solid';
 
 export default defineConfig({
   plugins: [solid()],
-})
+  base: './',
+  css: {
+    transformer: 'postcss',
+    modules: {
+      scopeBehaviour: 'local'
+    },
+    postcss: {
+      map: true
+    }
+  }
+});
